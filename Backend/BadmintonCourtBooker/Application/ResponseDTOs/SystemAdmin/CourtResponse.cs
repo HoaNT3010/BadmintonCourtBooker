8f6318863a5ac6b16f9c87215fae882ca6107d1b
haotnse150564 <anmay2k1@gmail.com>
2024-07-08 14:42:13 +0700

    Staff Services - kiểm tra trạng thái của sân mình đang quản lý

---
+++ Backend/BadmintonCourtBooker/Application/ResponseDTOs/SystemAdmin/CourtResponse.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ResponseDTOs.SystemAdmin
+{
+    public class CourtResponse
+    {
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public CourtType CourtType { get; set; } = CourtType.None;
+
+        public SlotType SlotType { get; set; } = SlotType.None;
+
+        public TimeSpan SlotDuration { get; set; } = TimeSpan.Zero;
+
+        public CourtStatus CourtStatus { get; set; } = CourtStatus.None;
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    }
+}
